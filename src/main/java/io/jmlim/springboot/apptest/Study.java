a80d7802cc6aff18db8efb821cf3893dbe860151
Lim Jeong muk <hackerljm@gmail.com>
2020-02-29 18:45:33 +0900

    Junit 5 테스트 반복하기 2부 추가

--- src/main/java/io/jmlim/springboot/apptest/Study.java
+++ src/main/java/io/jmlim/springboot/apptest/Study.java
@@ -4,10 +4,16 @@ public class Study {
     private StudyStatus status;
 
     private int limit;
+    private String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
 
     public Study(int limit) {
         // 만약 limit 값이 0보다 작을경우 exception 발생.
-        if(limit < 0) {
+        if (limit < 0) {
             throw new IllegalArgumentException("limit은 0보다 커야 합니다.");
         }
         this.limit = limit;
@@ -19,5 +25,18 @@ public class Study {
 
     public int getLimit() {
         return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", limit=" + limit +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/test/java/io/jmlim/springboot/apptest/StudyTest.java
+++ src/test/java/io/jmlim/springboot/apptest/StudyTest.java
@@ -2,10 +2,19 @@ package io.jmlim.springboot.apptest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // 테스트 이름 전략 짜기
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -28,14 +37,69 @@ class StudyTest {
     @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions}")
     void repeatTest(RepetitionInfo repetitionInfo) {
         // RepetitionInfo을 통해 반복된 정보들을 알 수 있다.
-        System.out.println("test" + repetitionInfo.getCurrentRepetition() +" / " + repetitionInfo.getTotalRepetitions());
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + " / " + repetitionInfo.getTotalRepetitions());
     }
 
-    @DisplayName("스터디 만들기 ParameterizedTest")
+    @DisplayName("스터디 만들기 ParameterizedTest String")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @ValueSource(strings = {"날씨가","많이","추워지고","있네요."})
+    @EmptySource // 비어있는 문자열을 인자로 추가.
+    @NullSource // 널을 인자로 추가.
+    @NullAndEmptySource // 널과 Empty 문자열을 인자로 추가.
     void parameterizedTest(String message) {
         System.out.println(message);
+    }
+
+    @DisplayName("스터디 만들기 ParameterizedTest Integer")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 40})
+    void parameterizedTest(Integer limit) {
+        System.out.println(limit);
+    }
+
+    @DisplayName("스터디 만들기 ParameterizedTest Study object (use StudyConverter)")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 40}) // 이경우는 숫자를 스터디 타입으로 받고자 하는것이기 때문에 컨버터를 만들어줘야 한다.
+    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+        System.out.println(study.getLimit());
+    }
+    static class StudyConverter extends SimpleArgumentConverter {
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    @DisplayName("스터디 만들기 ParameterizedTest Csv")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void parameterizedTestCsv(Integer limit, String name) {
+        Study study = new Study(limit, name);
+        System.out.println(study);
+    }
+
+    @DisplayName("스터디 만들기 ParameterizedTest Csv (use ArgumentsAccessor)")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void parameterizedTestCsv(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        System.out.println(study);
+    }
+
+    @DisplayName("스터디 만들기 ParameterizedTest Csv (use StudyAggregator)")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+    void parameterizedTestCsv(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+    // 2개의 인자값 사용 시 SimpleArgumentConverter 대신 ArgumentsAggregator 상속하여 만듬
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context)
+                throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
     }
 
     //반드시 스태틱 void 로 작성.
