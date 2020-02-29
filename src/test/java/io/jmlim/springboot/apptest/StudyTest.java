b96e62fea3f75124e102029249da5f045faa2292
Lim Jeong muk <hackerljm@gmail.com>
2020-02-29 16:19:44 +0900

    3. junit 5 assertion (assertAll 사용 예제)

--- src/test/java/io/jmlim/springboot/apptest/StudyTest.java
+++ src/test/java/io/jmlim/springboot/apptest/StudyTest.java
@@ -2,8 +2,6 @@ package io.jmlim.springboot.apptest;
 
 import org.junit.jupiter.api.*;
 
-import java.util.function.Supplier;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 // 테스트 이름 전략 짜기
@@ -14,23 +12,14 @@ class StudyTest {
     @DisplayName("스터디 만들기 \uD83D\uDE31")
     void create_new_study() {
         Study study = new Study(-10);
-        assertNotNull(study);
-        //  assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
-        //  assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
-        // 풀어보기
-        assertEquals(StudyStatus.DRAFT, study.getStatus(), new Supplier<String>() {
-            /**
-             * Gets a result.
-             *
-             * @return a result
-             */
-            @Override
-            public String get() {
-                return "스터디를 처음 만들면 DRAFT 상태여야 한다.";
-            }
-        });
-//        assertTrue(1 < 2);
-        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.");
+
+        // assertAll 로 각각의 테스트를 한번에 실행 할 경우 그 안에 실패한 케이스가 여러개가 있을 경우 한번에 볼 수 있다.
+        // assertAll 사용하지 않을 시 중간에 테스트가 실패날 경우 그 이후에 실패한 케이스가 있다해도 확인할 수 없었다.
+        assertAll(
+                () -> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야한다.")
+        );
     }
 
     @Test
