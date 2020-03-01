db07a9135e7c11fa4a0040b15d4cb735c5e12302
Lim Jeong muk <hackerljm@gmail.com>
2020-03-01 23:45:53 +0900

    Testcontainers, 도커 Compose 사용하기 2

--- src/test/java/io/jmlim/springboot/apptest/study/StudyServiceTest.java
+++ src/test/java/io/jmlim/springboot/apptest/study/StudyServiceTest.java
@@ -9,8 +9,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,6 +32,7 @@ import static org.mockito.Mockito.times;
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles("test")
 @Testcontainers
+@ContextConfiguration(initializers = StudyServiceTest.ContainerPropertyInitializer.class)
 class StudyServiceTest {
 
     @Mock
@@ -35,13 +41,19 @@ class StudyServiceTest {
     @Autowired
     StudyRepository studyRepository;
 
+    @Value("${container.port}")
+    int port;
+
     @Container
     static DockerComposeContainer composeContainer =
-             new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"));
-
+            new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"))
+                    .withExposedService("study-db", 5432);
 
     @Test
     void createNewStudy() {
+        System.out.println("===============");
+        System.out.println(port);
+
         // Given
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
@@ -77,5 +89,14 @@ class StudyServiceTest {
                 () -> assertNotNull(study.getOpenedDateTime())
         );
         then(memberService).should().notify(study);
+    }
+
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of("container.port=" + composeContainer.getServicePort("study-db", 5432))
+                    .applyTo(applicationContext.getEnvironment());
+        }
     }
 }
