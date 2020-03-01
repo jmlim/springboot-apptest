4a0d7ecb72c58d2b1189deefe07c32947fcdfb9e
Lim Jeong muk <hackerljm@gmail.com>
2020-03-01 14:01:57 +0900

    Mock 객체 stubbing 추가.  - Void 메소드 예외 호출 예제까지

--- src/test/java/io/jmlim/springboot/apptest/study/StudyServiceTest.java
+++ src/test/java/io/jmlim/springboot/apptest/study/StudyServiceTest.java
@@ -1,12 +1,18 @@
 package io.jmlim.springboot.apptest.study;
 
+import io.jmlim.springboot.apptest.domain.Member;
 import io.jmlim.springboot.apptest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 /**
  * 의존하고 있는 클래스에 대한 구현체는 없고 인터페이스만 있음.
@@ -19,7 +25,32 @@ class StudyServiceTest {
     @Test
     void createStudyService(@Mock MemberService memberService,
                             @Mock StudyRepository studyRepository) {
+
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("hackerljm@gmail.com");
+
+        // any()로 할경우 어떤 값을 던지더라도 위에서 만든 member 객체가 리턴
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+
+        assertEquals("hackerljm@gmail.com", memberService.findById(1L).get().getEmail());
+        assertEquals("hackerljm@gmail.com", memberService.findById(2L).get().getEmail());
+        assertEquals("hackerljm@gmail.com", memberService.findById(3L).get().getEmail());
+
+        // 1이라는 값을 던질경우 Throw 리턴
+        // when(memberService.findById(1L)).thenThrow(new RuntimeException());
+
+        // member메소드 중 validate 메소드가 1L 이라는 값을 던질경우 IllegalArgumentException 예외를 던짐.
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+
+        memberService.validate(2L);
+
     }
 }
