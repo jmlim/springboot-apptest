6199d4cd330ee2f2b1e226fcf93ef2c8932274bf
Lim Jeong muk <hackerljm@gmail.com>
2020-03-01 16:35:08 +0900

    연습문제 푼 후 추가.

--- src/test/java/io/jmlim/springboot/apptest/study/StudyServiceTest.java
+++ src/test/java/io/jmlim/springboot/apptest/study/StudyServiceTest.java
@@ -11,8 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
@@ -25,9 +24,13 @@ import static org.mockito.Mockito.times;
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
+    @Mock
+    MemberService memberService;
+    @Mock
+    StudyRepository studyRepository;
+
     @Test
-    void createStudyService(@Mock MemberService memberService,
-                            @Mock StudyRepository studyRepository) {
+    void createStudyService() {
 
         // Given
         StudyService studyService = new StudyService(memberService, studyRepository);
@@ -52,7 +55,7 @@ class StudyServiceTest {
 
         // Then
         assertEquals(member, study.getOwner());
-       // verify(memberService, times(1)).notify(study);
+        // verify(memberService, times(1)).notify(study);
         // 위 코드를 bdd 스타일로 작성.
         then(memberService).should(times(1)).notify(study);
         // memberService.notify(스터디) 실행 이후에 memberService의 notify(멤버); 를 한번 더 실행하므로 에러.
@@ -60,5 +63,28 @@ class StudyServiceTest {
         // verifyNoMoreInteractions(memberService);
         // 위코드를 bdd 스타일로 작성.
         then(memberService).shouldHaveNoMoreInteractions();
+    }
+
+    @DisplayName("다른 사용자가 볼 수 있도록 스터디를 공개한다.")
+    @Test
+    void openStudy() {
+        // Given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        Study study = new Study(10, "더 자바, 테스트");
+        // TODO studyRepository Mock 객체의 save 메소드를 호출 시 study를 리턴하도록 만들기.
+        given(studyRepository.save(study)).willReturn(study);
+
+        // When
+        studyService.openStudy(study);
+
+        // Then
+        // TODO study의 status가 OPENED로 변경됐는지 확인
+        // TODO study의 openedDataTime이 null이 아닌지 확인
+        assertAll(
+                () -> assertEquals(study.getStatus(), StudyStatus.OPENED),
+                () -> assertNotNull(study.getOpenedDateTime())
+        );
+        // TODO memberService의 notify(study)가 호출 됐는지 확인.
+        then(memberService).should(times(1)).notify(study);
     }
 }
